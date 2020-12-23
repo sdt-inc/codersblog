6209ee7b0108fc1ce0c65f30d8456e8b3ad55ac5
Nahid-Hassan <nahid.cseru@gmail.com>
2020-12-23 14:18:36 +0600

    create blog/tutorial.html and create tutorial views then map into it

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html', context={'index': "Index Page"})
+
+def tutorial(request):
+    return render(request, 'blog/tutorial.html', context={'tutorial' : 'Tutorial Page'})
