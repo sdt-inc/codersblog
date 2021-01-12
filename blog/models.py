99691d096faac800e7b7695b3080905f8b7dbf9a
Nahid-Hassan <nahid.cseru@gmail.com>
2021-01-12 22:13:21 +0600

    update base.html and others full-site section

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,9 @@ from django.db import models
 from django.shortcuts import reverse
 from django.utils import timezone
 from multiselectfield import MultiSelectField
-
+from django.db.models.signals import pre_save
 from .choice import keyword_choices, writer_choices
-
+from codersblog.utils import unique_slug_generator
 
 class PythonDB(models.Model):
     # define your model here
@@ -27,9 +27,18 @@ class PythonDB(models.Model):
     body = RichTextUploadingField(config_name='default', blank=True, null=True)
     related_page_link = models.CharField(max_length=200)
 
+    slug = models.SlugField(max_length=200, null=True, blank=True)
+
     def __str__(self):
         return self.title
 
     # slug, sitemap
     def get_absolute_url(self):
         return reverse('tutorial', args=[str(self.id)])  # new
+
+def slug_generator(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# everytime the PythonDB class object is created this below method is executed.
+pre_save.connect(slug_generator, sender=PythonDB)
